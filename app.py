13cf15dc811f6e4ddeacb6da6b2964053975f571
prakashjais <167881309+prakashjais@users.noreply.github.com>
2024-09-05 14:13:22 +0530

    Update app.py
     admin and teacher route removed

--- app.py
+++ app.py
@@ -226,13 +226,6 @@ def get_course():
 def get_exam_questions(exam_id):
     return db.session.execute(text("SELECT * FROM exam_questions WHERE course_id = :exam_id"), {'exam_id': exam_id}).fetchall()
 
-@app.route("/adminclick")
-def adminclick():
-    return render_template("exam/index.html")
-
-@app.route("/teacherclick")
-def teacherclick():
-    return render_template("exam/index.html")
 
 @app.route("/aboutus")
 def aboutus():
